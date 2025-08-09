5879d6dd8ac0194495d754b08c3f4f5883f870fd
Yassin-Elmahdy123 <yassinelmahdy201@gmail.com>
2025-08-09 22:34:32 +0300

    Add Dark Mode Feature
    I added the Dark Mode Feature to the three pages by modifying HTML, CSS, and JS files.

---
+++ index.js
@@ -0,0 +1,27 @@
+//Check if currentmode is present or not to set it
+if(localStorage.getItem("currentmode") === null) {
+    localStorage.setItem("currentmode", "false");
+}
+let style = document.getElementById("theme-style");
+
+CheckMode();
+function CheckMode() {
+    if(localStorage.getItem("currentmode") === "true") {
+        // If currentmode is true, switch to dark mode
+        style.setAttribute("href", "style-dark.css");
+    } else {
+        // If currentmode is false, switch to light mode
+        style.setAttribute("href", "style.css");
+    }
+}
+
+function ConvertMode() {
+    // Toggle the currentmode value and update the stylesheet link
+    if(localStorage.getItem("currentmode") === "true") {
+        localStorage.setItem("currentmode", "false");
+        CheckMode();
+    }else {
+        localStorage.setItem("currentmode", "true");
+        CheckMode();
+    }
+}
